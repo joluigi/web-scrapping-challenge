9e5c1dad21f07b9255e38166a56e69f7bfc93656
Jose Luis <jolui334@gmail.com>
2021-04-11 18:02:03 -0500

    First attempt to load all collected data into the index.html file

--- Mission_to_mars/scrape_mars_flask.py
+++ Mission_to_mars/scrape_mars_flask.py
@@ -1,12 +1,30 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_phone
+import scrape_mars
 
 
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_scrp_db")
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db_scraping"
+mongo =PyMongo(app)
+#mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_scrp_db")
+mongo.db.mars_data.drop()
+
 
 @app.route("/")
-def index():
-    listings 
+def indexroute():
+    ft_img = mongo.db.mars_data.find_one()
+    # ft_img = "https://spaceimages-mars.com/image/featured/mars2.jpg"
+    output = render_template("index.html", fimage=ft_img)
+    return output
+
+@app.route("/scrape")
+def scraper():
+    mars_list = scrape_mars.mars_scraping()
+    mongo.db.mars_data.update({}, mars_list, upsert=True) 
+    return redirect("/", code = 302)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
